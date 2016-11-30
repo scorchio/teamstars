a633264e0287fa028e7fca54ea282379a1d051d5
Zoltan Adamek <zoltan.adamek@prezi.com>
2016-11-30 19:08:24 +0100

    Fixes code style.

--- teamstars/teamstars/urls.py
+++ teamstars/teamstars/urls.py
@@ -15,4 +15,4 @@ urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^api/', include(router.urls)),
                        url(r'^api-auth/', include('rest_framework.urls')),
-)
+                       )
