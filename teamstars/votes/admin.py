47ddadf4f420d8bec18d08ecb06d2510a09e89b0
Zoltan Adamek <zoltan.adamek@prezi.com>
2016-11-30 18:03:44 +0100

    Adjusts admin interface for votes.

--- teamstars/votes/admin.py
+++ teamstars/votes/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from models import Vote, VoteType
 
-admin.site.register(Vote)
+
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ['title', 'type', 'sender', 'recipient', 'created']
+    list_filter = ['created']
+    search_fields = ['title']
+
+admin.site.register(Vote, VoteAdmin)
 admin.site.register(VoteType)
