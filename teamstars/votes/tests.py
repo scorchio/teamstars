cde6b1e512e3b639e4b04466b9047a54d5fd9986
Zoltan Adamek <zoltan.adamek@prezi.com>
2016-11-30 13:21:15 +0100

    Starts adding unit tests for voting.

--- teamstars/votes/tests.py
+++ teamstars/votes/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.core.exceptions import ObjectDoesNotExist
+from models import VoteType, Vote
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class VoteTestCase(TestCase):
+    def test_user_creation_delete(self):
+        """Should be able to create / delete users"""
+        user1 = User.objects.create_user(username="user1")
+        user2 = User.objects.create_user(username="user2")
+        self.assertEqual(2, User.objects.count(), "Couldn't create users")
+        user1.delete()
+        user2.delete()
+        self.assertEqual(0, User.objects.count(), "Couldn't delete users")
+
+    def test_vote_type_creation_delete(self):
+        """Should be able to create / delete vote types"""
+        vote_type_1 = VoteType.objects.create(type="+")
+        vote_type_2 = VoteType.objects.create(type="-")
+        self.assertEqual(2, VoteType.objects.count(), "Couldn't create vote types")
+        vote_type_1.delete()
+        vote_type_2.delete()
+        self.assertEqual(0, VoteType.objects.count(), "Couldn't delete vote types")
+
+    def test_vote_creation_delete(self):
+        """Should be able to create a vote"""
+        user1 = User.objects.create_user(username="user1")
+        user2 = User.objects.create_user(username="user2")
+        VoteType.objects.create(type="+")
+
+        with self.assertRaises(ObjectDoesNotExist, msg="Did not raise exception as expected"):
+            bogus_vote_type = VoteType.objects.get(type="-")
+
+        working_vote_type = VoteType.objects.get(type="+")
+
+        Vote.objects.create(sender=user1, recipient=user2, type=working_vote_type, description="Test vote")
+        self.assertEqual(1, Vote.objects.count(), "Couldn't create vote")
