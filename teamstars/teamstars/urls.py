c6886b26bf8ff258557fc200b4b099ddc72ea914
Zoltan Adamek <zoltan.adamek@prezi.com>
2016-11-30 18:47:04 +0100

    Starts adding django-rest-framework.

--- teamstars/teamstars/urls.py
+++ teamstars/teamstars/urls.py
@@ -1,10 +1,44 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.models import User
+
+from rest_framework import routers, serializers, viewsets
+
+from votes.models import Vote
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class VoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = ('id', 'type', 'sender', 'recipient', 'title', 'description',
+                  'created',
+                  'modified')
+
+
+class VoteViewSet(viewsets.ModelViewSet):
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'votes', VoteViewSet)
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'teamstars.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^api/', include(router.urls)),
+                       url(r'^api-auth/', include('rest_framework.urls')),
 )
