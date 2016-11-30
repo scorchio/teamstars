4ded7b6ca51bc38fa154dc8559d663d071d45588
Zoltan Adamek <zoltan.adamek@prezi.com>
2016-11-30 19:07:15 +0100

    Adds VoteType to the REST API so hyperlinked serializers will fully work.

--- teamstars/teamstars/serializers.py
+++ teamstars/teamstars/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 
 from rest_framework import serializers, viewsets
 
-from votes.models import Vote
+from votes.models import Vote, VoteType
 
 
 # Serializers define the API representation.
@@ -18,14 +18,24 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
-class VoteSerializer(serializers.ModelSerializer):
+class VoteSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Vote
         fields = ('id', 'type', 'sender', 'recipient', 'title', 'description',
-                  'created',
-                  'modified')
+                  'created', 'modified')
 
 
 class VoteViewSet(viewsets.ModelViewSet):
     queryset = Vote.objects.all()
     serializer_class = VoteSerializer
+
+
+class VoteTypeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = VoteType
+        fields = ('id', 'type', 'created', 'modified')
+
+
+class VoteTypeViewSet(viewsets.ModelViewSet):
+    queryset = VoteType.objects.all()
+    serializer_class = VoteTypeSerializer
--- teamstars/teamstars/urls.py
+++ teamstars/teamstars/urls.py
@@ -3,12 +3,13 @@ from django.contrib import admin
 
 from rest_framework import routers
 
-from serializers import UserViewSet, VoteViewSet
+from serializers import UserViewSet, VoteViewSet, VoteTypeViewSet
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'votes', VoteViewSet)
+router.register(r'votes-types', VoteTypeViewSet)
 
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
