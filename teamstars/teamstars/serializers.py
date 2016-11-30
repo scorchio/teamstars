86440e3fdd7181656e3593e2520ddcd70dd52bb5
Zoltan Adamek <zoltan.adamek@prezi.com>
2016-11-30 18:50:54 +0100

    Moves serializers and viewsets into the serializers file.

---
+++ teamstars/teamstars/serializers.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User
+
+from rest_framework import serializers, viewsets
+
+from votes.models import Vote
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class VoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = ('id', 'type', 'sender', 'recipient', 'title', 'description',
+                  'created',
+                  'modified')
+
+
+class VoteViewSet(viewsets.ModelViewSet):
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
--- teamstars/teamstars/urls.py
+++ teamstars/teamstars/urls.py
@@ -1,36 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.contrib.auth.models import User
 
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers
 
-from votes.models import Vote
-
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class VoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Vote
-        fields = ('id', 'type', 'sender', 'recipient', 'title', 'description',
-                  'created',
-                  'modified')
-
-
-class VoteViewSet(viewsets.ModelViewSet):
-    queryset = Vote.objects.all()
-    serializer_class = VoteSerializer
+from serializers import UserViewSet, VoteViewSet
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
