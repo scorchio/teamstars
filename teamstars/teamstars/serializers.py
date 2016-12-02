cf310e9a7ce1db8c01c68f0c49d7197c114cbf6d
Zoltan Adamek <zoltan.adamek@prezi.com>
2016-12-02 11:18:57 +0100

    Cleans up unnecessary comments.

--- teamstars/teamstars/serializers.py
+++ teamstars/teamstars/serializers.py
@@ -5,14 +5,12 @@ from rest_framework import serializers, viewsets
 from votes.models import Vote, VoteType
 
 
-# Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'is_staff')
 
 
-# ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
