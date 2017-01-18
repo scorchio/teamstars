456f66c1e13150d7710c06334b42fbd59f3addc3
Zoltán Adamek <zoltan.adamek@prezi.com>
2017-01-18 11:41:21 +0100

    Tries fixing issue with relative imports.

--- teamstars/teamstars/wsgi.py
+++ teamstars/teamstars/wsgi.py
@@ -11,6 +11,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 sys.path = [os.path.abspath(os.path.dirname(__file__))] + sys.path
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "teamstars.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 application = get_wsgi_application()
