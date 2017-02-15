f49eefddf91e9b839dcad578c5e27832c2c6b353
Zoltán Adamek <zoltan.adamek@prezi.com>
2017-02-15 18:35:13 +0100

    Modifies .env file handling to include switch for SQLite; adds example .env file.

--- teamstars/teamstars/settings.py
+++ teamstars/teamstars/settings.py
@@ -86,13 +86,38 @@ WSGI_APPLICATION = 'teamstars.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'RDS_DB_NAME' in os.environ:
+    # Use production settings
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+elif 'USE_SQLITE' in os.environ and os.environ['USE_SQLITE'] == 'True':
+    # Use lightweight dev env
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    # Use dev env with postgre
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'teamstars',
+            'USER': 'teamstars',
+            'PASSWORD': 'teamstars',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
