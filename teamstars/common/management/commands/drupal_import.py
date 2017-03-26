89852d1088bdd897b9c9d024e5bba3867a258f8d
Zoltán Adamek <zoltan.adamek@prezi.com>
2017-03-26 17:39:16 +0200

    Improves import management command a bit.

--- teamstars/common/management/commands/drupal_import.py
+++ teamstars/common/management/commands/drupal_import.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.db import connections
 
 
@@ -9,22 +9,28 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         with connections['legacy'].cursor() as cursor:
-            cursor.execute("select u.name, u.mail, u.created, "
+            cursor.execute("select u.uid, u.name, u.mail, u.created, u.status, "
                            "r.name as role, "
-                           "fn.field_user_full_name_value as fullname "
+                           "fn.field_user_full_name_value as fullname, "
+                           "fm.filename as picture "
                            "from users u left join field_data_field_user_full_name fn on u.uid = fn.entity_id "
-                           "left join (users_roles ur left join role r on ur.rid=r.rid) on u.uid=ur.uid"
+                           "left join (users_roles ur left join role r on ur.rid=r.rid) on u.uid=ur.uid "
+                           "left join file_managed fm on u.picture = fm.fid"
                            )
             columns = [col[0] for col in cursor.description]
             result = [dict(zip(columns, row)) for row in cursor.fetchall()]
             for user in result:
-                self.stdout.write(u'User: {name} ({fullname}) <{mail}> - {created} - {role}'.format(
-                    mail=user['mail'],
-                    created=datetime.utcfromtimestamp(user['created']),
-                    name=user['name'],
-                    fullname=user['fullname'],
-                    role=user['role']
-                ))
+                # Let's ignore Drupal root, as we shouldn't move that user
+                if user['uid'] != 0:
+                    self.stdout.write(u'User: {name} ({fullname}) <{mail}> - {created} - {role} - {picture} - {status}'.format(
+                        mail=user['mail'],
+                        created=datetime.utcfromtimestamp(user['created']),
+                        name=user['name'],
+                        fullname=user['fullname'],
+                        role=user['role'],
+                        picture=user['picture'],
+                        status=user['status']
+                    ))
 
         # TODO: upgrade to Django 1.9+ to support styling in management commands
         self.stdout.write('Successfully run command')
