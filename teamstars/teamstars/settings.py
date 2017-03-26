a2be3f13fb77526413abe4329a54eb80498197cf
Zoltán Adamek <zoltan.adamek@prezi.com>
2017-03-26 17:49:36 +0200

    Fixes template path handling.

--- teamstars/teamstars/settings.py
+++ teamstars/teamstars/settings.py
@@ -20,7 +20,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', False)
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
