3ba281f910eea341e3f4aee473463d0d0272c675
Zoltán Adamek <zoltan.adamek@prezi.com>
2017-04-03 11:19:49 +0200

    Adds logging to bot start view

--- teamstars/teamstars/bot_handlers.py
+++ teamstars/teamstars/bot_handlers.py
@@ -1,8 +1,12 @@
 from telegrambot.bot_views.generic import TemplateCommandView
 from telegrambot.handlers import command
 
+import logging
+logger = logging.getLogger(__name__)
+
 
 class StartView(TemplateCommandView):
+    logger.debug("Incoming start view!")
     template_text = "bot/messages/command_start_text.txt"
 
 urlpatterns = [
