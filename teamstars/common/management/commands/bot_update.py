c271f217f3eb69f3580d70b6a8ed6db9c9ab2b51
Zoltán Adamek <zoltan.adamek@prezi.com>
2017-04-03 19:24:54 +0200

    Just a very bad way to test how this cron-based flow would look like

---
+++ teamstars/common/management/commands/bot_update.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+
+import kronos
+from telegram import ParseMode
+from telegrambot.models import Bot
+
+
+@kronos.register('0 * * * *')
+class Command(BaseCommand):
+    help = 'Sends updates to Telegram bot.'
+
+    def handle(self, *args, **options):
+        bot = Bot.objects.first()
+        bot.send_message(chat_id=70371207, text="I'm still here ^^", reply_markup=None, parse_mode=ParseMode.MARKDOWN)
+
